21f3c507c567a9dc32bbe280a9001990820e87be
Jules <julian.debhal@gmail.com>
2014-06-12 18:35:45 +1000

    increment figwheel version

--- project.clj
+++ project.clj
@@ -5,13 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2202"]
-                 [figwheel "0.1.2-SNAPSHOT"]
+                 [figwheel "0.1.3-SNAPSHOT"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [sablono "0.2.16"]
                  [om "0.6.2"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-figwheel "0.1.2-SNAPSHOT"]
+            [lein-figwheel "0.1.3-SNAPSHOT"]
             [com.cemerick/clojurescript.test "0.3.0"]]
 
   :cljsbuild {
